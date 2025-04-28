cb0f44c2429786c143e9402f7ff7a34661309069
Narsapur Prashanth <prashanth.narsapur@cloud4c.com>
2025-04-28 14:53:39 +0530

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from NodeJS App running on EKS!');
+  res.send('Hello from NodeJS App running on Ecs!');
 });
 
 app.listen(port, () => {
