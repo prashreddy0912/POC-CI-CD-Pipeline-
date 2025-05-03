626a064cb1c98ee04328445bcdf3d80b44442284
Narsapur Prashanth <prashanth.narsapur@cloud4c.com>
2025-05-03 22:16:05 +0530

    ecr

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from NodeJS App running on ECS');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running at http://localhost:${port}`);
 });
--- pipeline.tf
+++ pipeline.tf
@@ -213,11 +213,11 @@ resource "aws_ecs_service" "my-app-test" {
   desired_count   = 1
   launch_type     = "FARGATE"
 
-  network_configuration {
-    subnets          = ["subnet-0dcad7a2a716631be", "subnet-0d1f057476b350364"]
-    assign_public_ip = true
-    security_groups  = ["sg-0e10486d3b9954656"]
-  }
+network_configuration {
+  subnets          = module.vpc.private_subnets
+  assign_public_ip = true
+  security_groups  = [aws_security_group.ecs_service_sg.id]
+}
 
   # load_balancer {
   #   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:390402565417:targetgroup/my-target-group/1234567890abcdef"
@@ -294,4 +294,52 @@ resource "aws_codepipeline" "github_pipeline" {
       }
     }
   }
+}
+resource "aws_security_group" "ecs_service_sg" {
+  name        = "ecs-service-sg"
+  description = "Security group for ECS service"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere (adjust as needed)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+  }
+}
+resource "aws_ecs_task_definition" "app-task" {
+  family                   = "my-app-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = jsonencode([
+    {
+      name      = "my-app-task",
+      image     = "${aws_ecr_repository.app.repository_url}:latest",
+      essential = true,
+      portMappings = [
+        {
+          containerPort = 3000,
+          protocol      = "tcp"
+        }
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/my-app-task"
+          awslogs-region        = "us-east-1"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
 }
